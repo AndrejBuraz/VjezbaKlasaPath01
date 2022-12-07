1d6ed7628e9de881efe8f2d9cd39d9e43695493e
Andrej <75538726+AndrejBuraz@users.noreply.github.com>
2022-12-07 09:26:18 +0100

    Add files via upload

---
+++ VjezbaKlasaPath01/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+namespace PathKlasa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Unesite putanju izvornog direktorija: ");
+            string izvorniDirektorij = Console.ReadLine();
+            Console.WriteLine("Unesite putanju ciljanog direktorija: ");
+            string ciljniDirektorij = Console.ReadLine();
+            try
+            {
+                foreach(string datoteka in Directory.GetFiles(izvorniDirektorij))
+                {
+                    string imeDatoteke = Path.GetFileName(datoteka);
+                    string ciljnaDatoteka = Path.Combine(ciljniDirektorij, imeDatoteke);
+                    File.Copy(datoteka, ciljnaDatoteka, true);
+                }
+                Console.WriteLine("Gotovo!");
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Greška: {0}", ex.Message);
+            }
+            Console.ReadKey();
+        }
+    }
+
+}
